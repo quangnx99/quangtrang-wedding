104e02a2b143f58b3a0ad4927d999b4c4301ea11
quangnx <adquang199x@gmail.com>
2025-02-06 16:31:19 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ['./src/**/*.{html,js}'], // Điều chỉnh đường dẫn nếu cần
+    theme: {
+        extend: {
+            fontFamily: {
+                'playwrite': "'Playwrite IN', cursive;",
+            },
+        },
+    },
+    plugins: [],
+};  
